38ed9bbea4881924f6e8b4cb7161f5520dd4fb0a
depedisabelaaral <markiecabildo@gmail.com>
2025-12-11 20:56:51 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+const CACHE_NAME = 'aral-cache-v1';
+const urlsToCache = ['./','./index.html','./Logo.png','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css','https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'];
+
+self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))); });
+self.addEventListener('activate', e=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
